ff0045774a917b54b78423ef6ce2b61404b55862
William5920 <1101044179@qq.com>
2024-12-29 17:25:08 +0800

    css重置和less支持

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,13 @@
 const path = require('path')
+const CracoLessPlugin = require('craco-less')
 
-const resolve = dir => path.resolve(__dirname, dir)
+const resolve = (dir) => path.resolve(__dirname, dir)
 
 module.exports = {
+  plugins: [{ plugin: CracoLessPlugin }],
   webpack: {
-      alias: {
-        '@': resolve('src')
-      }
+    alias: {
+      '@': resolve('src')
+    }
   }
 }
